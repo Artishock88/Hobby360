6c38258ad7991f3a228d196bd857e40ba7618fe5
Artishock88 <artyom.tokarev@art-tokarev.de>
2015-04-14 09:26:00 +0200

    panoviewer neu plus higher resolution

--- app/src/main/java/de/artmedia/artyom/hobby360/Panoviewer.java
+++ app/src/main/java/de/artmedia/artyom/hobby360/Panoviewer.java
@@ -1,5 +1,6 @@
 package de.artmedia.artyom.hobby360;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
@@ -12,6 +13,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Surface;
 import android.view.WindowManager;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.panoramagl.PLImage;
@@ -26,23 +28,21 @@ import java.util.List;
 /**
  * Created by Artyom on 06.03.2015.
  */
-public class Panoviewer extends PLView {
+    public class Panoviewer extends PLView
+    {
+        public void onCreate (Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+
+            this.load(new PLJSONLoader("res://raw/json_spherical"));
+            PLSpherical2Panorama panorama = new PLSpherical2Panorama();
+            Intent intentExtras = getIntent();
+            int pano = getIntent().getExtras().getInt("pano");
 
 
+            panorama.setImage(new PLImage(PLUtils.getBitmap(this, pano), false));
+            this.setPanorama(panorama);
+        }
 
-public void onCreate (Bundle savedInstanceState)
-{
-    super.onCreate(savedInstanceState);
-    this.load(new PLJSONLoader("res://raw/json_spherical"));
-    PLSpherical2Panorama panorama = new PLSpherical2Panorama();
-    Intent intentExtras = getIntent();
-    int pano = getIntent().getExtras().getInt("pano");
+    }
 
-
-    panorama.setImage(new PLImage(PLUtils.getBitmap(this, pano), false));
-    this.setPanorama(panorama);
-
-}
-
-
-}
