e8f1edf18d7f5c85590faa0f5c5341962d4088aa
Artishock88 <artyom.tokarev@art-tokarev.de>
2015-04-25 16:18:45 +0200

    loading JSON in Panoviewer

--- app/src/main/java/de/artmedia/artyom/hobby360/Panoviewer.java
+++ app/src/main/java/de/artmedia/artyom/hobby360/Panoviewer.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.hardware.Sensor;
@@ -11,15 +12,18 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.JsonReader;
 import android.util.Log;
 import android.view.Surface;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.panoramagl.PLIPanorama;
+import com.panoramagl.PLIView;
 import com.panoramagl.PLImage;
 import com.panoramagl.PLSpherical2Panorama;
 import com.panoramagl.PLView;
@@ -28,7 +32,14 @@ import com.panoramagl.loaders.PLJSONLoader;
 import com.panoramagl.transitions.PLTransitionBlend;
 import com.panoramagl.utils.PLUtils;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.InputStream;
 import java.util.List;
+
+import static de.artmedia.artyom.hobby360.R.drawable.pano_rotation_2d_deactive;
 
 
 /**
@@ -40,11 +51,21 @@ import java.util.List;
         String pano;
         //PLSpherical2Panorama panorama = new PLSpherical2Panorama();
         PLILoader panorama = null;
+        //private JSONObject jsonObject;
+        //private String strJSONValueOf = "\"sensorialRotation\": true,";
+        private JSONObject json;
+        private JSONObject accelerometer;
+        private Context context;
 
         public void onCreate (Bundle savedInstanceState)
         {
             super.onCreate(savedInstanceState);
-
+            try {
+                screenRot();
+            } catch (JSONException e) {
+                e.printStackTrace();
+                Toast.makeText(this.getApplicationContext(), "your dev was an idiot",Toast.LENGTH_SHORT).show();
+            }
 
             /*PLSpherical2Panorama panorama = new PLSpherical2Panorama();
             this.load(new PLJSONLoader("res://raw/json_spherical"));
@@ -56,6 +77,7 @@ import java.util.List;
             panorama.setImage(new PLImage(PLUtils.getBitmap(this, pano), false));
             this.setPanorama(panorama);*/
         }
+
         /*protected void onPause()
         {
             super.onPause();
@@ -67,6 +89,109 @@ import java.util.List;
             super.onResume();
             loadPanorama();
         }*/
+
+        public void JSONLoader (Context context, String url) throws JSONException {
+            try
+            {
+                url = url.trim();
+                InputStream is = null;
+                if(url.indexOf("res://")==0)
+                {
+                    int sepPos = url.lastIndexOf("/");
+                    int resId = context.getResources().getIdentifier(url.substring(sepPos+1),url.substring(6, sepPos),context.getPackageName());
+                    is = context.getResources().openRawResource(resId);
+                }
+                else
+                is = context.openFileInput(url);
+                byte[] bytes = new byte[is.available()];
+                is.read(bytes);
+                is.close();
+                this.loadJSON(new String(bytes, "utf-8"));
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+                throw new RuntimeException(e.getMessage(), e);
+            }
+
+        }
+
+        protected void loadJSON(String jsonString)
+        {
+            Toast.makeText(this.getApplicationContext(),jsonString, Toast.LENGTH_LONG).show();
+            if(jsonString!=null)
+            {
+                try {
+                    json = new JSONObject(jsonString);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    throw new RuntimeException("JSON parse failed", e);
+                }
+            }
+            else
+                throw new RuntimeException("JSON string is empty");
+        }
+
+        public int getScreenOrientation()
+        {
+            int config = getResources().getConfiguration().orientation;
+            return config;
+        }
+
+        private void screenRot() throws JSONException {
+            final Button rot3d = (Button) findViewById(R.id.pano_rotation_3d);
+            final Button rot2d = (Button) findViewById(R.id.pano_rotation);
+
+
+            switch (getScreenOrientation())
+            {
+                case (0):
+                    rot2d.setBackground(getResources().getDrawable(R.drawable.pano_rotation_2d));
+                    rot2d.setEnabled(true);
+                    rot3d.setBackground(getResources().getDrawable(R.drawable.pano_rotation));
+                    rot3d.setEnabled(true);
+
+                    break;
+                case (1):
+                    rot2d.setBackground(getResources().getDrawable(R.drawable.pano_rotation_2d_deactive));
+                    rot2d.setEnabled(false);
+                    rot3d.setBackground(getResources().getDrawable(R.drawable.pano_rotation_deactive));
+                    rot3d.setEnabled(false);
+
+
+                    pano = getIntent().getExtras().getString("pano");
+                    JSONLoader(this, pano);
+
+                    //The accelerometer has those values, as all if-statements are true (tried with Toast.)
+                    accelerometer = json.getJSONObject("accelerometer");
+                    try {
+                        if (accelerometer.has("enabled")) {
+                            accelerometer.put("enabled", true);
+                        }
+                        if (accelerometer.has("leftRightEnabled")) {
+                            accelerometer.put("leftRightEnabled", true);
+                        }
+                        if (accelerometer.has("upDownEnabled")) {
+                            accelerometer.put("upDownEnabled", true);
+                        }
+                    }catch (JSONException e)
+                    {
+                        e.printStackTrace();
+                    }
+                    loadPanoramaFromJson();
+                    break;
+                case (2):
+                    rot2d.setBackground(getResources().getDrawable(R.drawable.pano_rotation_2d));
+                    rot2d.setEnabled(true);
+                    rot3d.setBackground(getResources().getDrawable(R.drawable.pano_rotation));
+                    rot3d.setEnabled(true);
+
+                    pano = getIntent().getExtras().getString("pano");
+                    JSONLoader(this, pano);
+
+                    break;
+            }
+        }
 
         public void onPause()
         {
@@ -80,36 +205,21 @@ import java.util.List;
             ViewGroup mainView = (ViewGroup)this.getLayoutInflater().inflate(R.layout.panoviewer, null);
             mainView.addView(contentView,0);
 
-            this.loadPanoramaFromJson();
+            //this.loadPanoramaFromJson();
 
             return super.onContentViewCreated(mainView);
         }
 
-        //@SuppressWarnings("unused")
         private void loadPanoramaFromJson()
         {
             try
             {
                 Context context = this.getApplicationContext();
                 pano = getIntent().getExtras().getString("pano");
-                //this.load(new PLJSONLoader(pano));
+                panorama = new PLJSONLoader(pano);
 
-                Toast.makeText(this.getApplicationContext(), pano, Toast.LENGTH_SHORT).show();
-
-                /*panorama = new PLSpherical2Panorama();
-                panorama.setImage(new PLImage(PLUtils.getBitmap(context, pano), false));
-
-                if(panorama!=null)
-                {
-                    panorama.getCamera().lookAt(0.0f,170.0f);
-                    this.reset();
-                    this.setPanorama(panorama);
-                }else {Toast.makeText(this.getApplicationContext(), "no pano", Toast.LENGTH_SHORT).show();}
-                this.setLocked(false);*/
-                panorama = new PLJSONLoader("res://raw/json_k55");
                 if(panorama!=null)
                     this.load(panorama, true, new PLTransitionBlend(2.0f));
-
 
 
             }
@@ -118,6 +228,29 @@ import java.util.List;
                 Toast.makeText(this.getApplicationContext(), "Fehler: "+e, Toast.LENGTH_SHORT).show();
             }
         }
+
+        public void onBackClick(View view)
+        {
+            switch(view.getId())
+            {
+                case(R.id.pano_back):
+                    super.onBackPressed();
+                    break;
+            }
+
+        }
+
+        public void on3dActive(View view)
+        {
+            switch (view.getId())
+            {
+                case(R.id.pano_rotation_3d):
+
+                    break;
+            }
+        }
+
+
 
     }
 
